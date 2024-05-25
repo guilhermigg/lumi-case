d8c982f7c054f56cd6f348f238cf491a3be151f1
guilhermigg <guilhermigg.e@gmail.com>
2024-05-25 07:54:46 -0300

    new: tema escuro por padrão

--- lumi-frontend/src/main.tsx
+++ lumi-frontend/src/main.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      paper: '#002E20'
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline enableColorScheme/>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline enableColorScheme/>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
