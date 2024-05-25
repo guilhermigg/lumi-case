f7169e580b8454b5f767790e817e5d35cc4c3562
guilhermigg <guilhermigg.e@gmail.com>
2024-05-25 07:50:17 -0300

    update: extraindo todos os dados necessários da fatura

--- lumi-api/src/helpers/parsePDFData.ts
+++ lumi-api/src/helpers/parsePDFData.ts
@@ -1,25 +1,68 @@
+import RegExHandler from "./regexHandler";
+
 interface IPDFData {
     customerNumber: string | null;
     referenceMonth?: string;
 
-    eletricityKwh?: string;
-    eletricityPrice?: string;
+    electricityKwh?: string;
+    electricityPrice?: string;
 
-    eletricitySCEEKwh?: string;
-    eletricitySCEEPrice?: string;
+    electricitySCEEKwh?: string;
+    electricitySCEEPrice?: string;
 
-    eletricityGDIKwh?: string;
-    eletricityGDIPrice?: string;
+    electricityGDIKwh?: string;
+    electricityGDIPrice?: string;
 
     municipalPublicLightingContribution?: string;
+    originalText?: string;
 }
 
-export default function(text : string) : IPDFData {
-    const regex : RegExp = /N[ºo]\s*DA\s*INSTALAÇÃO\\n\s*(\d{10})/i;
-    const match = regex.exec(text);
-    const customerNumber : string | null = match ? match[1] : null;
+export default function(text : string) : IPDFData | object {
+    const regexHandler = new RegExHandler(text);
 
-    return {
-        customerNumber
+    const result : IPDFData = {
+        customerNumber: "",
     }
+
+    // Número do Cliente
+    const customerNumber = regexHandler.exec('customerNumber', 0);
+    if(!customerNumber)
+        throw new Error("Número do cliente não identificado");
+
+    result.customerNumber = customerNumber;
+
+    // Mês de Referência
+    const referenceMonth = regexHandler.exec('referenceMonth', 0);
+    if(!referenceMonth)
+        throw new Error("Mês de referência não encontrado")
+
+    result.referenceMonth = referenceMonth;
+
+    // Energia Elétrica
+    const electricity : string | null = regexHandler.exec('electricity', 0);
+
+    const electricityArrayNumbers = regexHandler.onlyNumbers(electricity);
+    result.electricityKwh = electricityArrayNumbers[0] || ""; 
+    result.electricityPrice = electricityArrayNumbers[1] || ""; 
+
+    // Energia SCEE Isenta
+    const electricitySCEE : string | null = regexHandler.exec('electricitySCEE', 0);
+
+    const electricitySCEENumbers = regexHandler.onlyNumbers(electricitySCEE);
+    result.electricitySCEEKwh = electricitySCEENumbers[0] || ""; 
+    result.electricitySCEEPrice = electricitySCEENumbers[1] || ""; 
+
+    // Energia compensada GDI I
+    const electricityGDI : string | null = regexHandler.exec('electricityGDI', 0);
+
+    const electricityGDIArrayNumbers = regexHandler.onlyNumbers(electricityGDI) 
+    result.electricityGDIKwh = electricityGDIArrayNumbers[0] || ""; 
+    result.electricityGDIPrice = electricityGDIArrayNumbers[1] || ""; 
+
+    // Contribuição Iluminação Pública Municipal
+    const municipalContribution : string | null = regexHandler.exec('municipal', 0);
+    const municipalArrayNumbers = regexHandler.onlyNumbers(municipalContribution);
+    result.municipalPublicLightingContribution = municipalArrayNumbers[0] || ""; 
+
+    return result
 }
