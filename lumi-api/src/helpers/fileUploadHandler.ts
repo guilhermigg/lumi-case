f37d7d80469b5369b259cf57ae038c66211d9611
guilhermigg <guilhermigg.e@gmail.com>
2024-05-25 07:51:29 -0300

    update: alteração no método de error handling do upload de arquivos

--- lumi-api/src/helpers/fileUploadHandler.ts
+++ lumi-api/src/helpers/fileUploadHandler.ts
@@ -2,22 +2,16 @@ import { UploadedFile } from 'express-fileupload';
 import path from 'path';
 
 interface IObjetoRetorno {
-    error?: string,
     path?: string
 }
 
 export default async function handleUpload(files : any) : Promise<IObjetoRetorno> {
     if (!files || Object.keys(files).length === 0)
-        return { "error": "Nenhum arquivo enviado" }
+        throw new Error("Nenhum arquivo enviado");
 
     const file = files.file as UploadedFile;
     const uploadPath = path.join(__dirname, '..', 'uploads', file.name);
 
-    try{
-      await file.mv(uploadPath);
-      return { "path": uploadPath }
-    } catch (e) {
-      console.log("[ File upload ]", e);
-      return { "error": "Não foi possível fazer o upload do arquivo" }
-    }
+    await file.mv(uploadPath);
+    return { "path": uploadPath }
 }
--- lumi-api/src/routers/api.ts
+++ lumi-api/src/routers/api.ts
@@ -6,23 +6,20 @@ import parsePDFData from '../helpers/parsePDFData';
 const apiRouter = express.Router();
 
 apiRouter.get('/healthcheck', (req : Request, res : Response) => {
-    
     return res.sendStatus(200);
 });
 
 apiRouter.post('/analysis', async (req : Request, res: Response) => {
-    const uploadResult = await handleUpload(req.files);
+    try{
+        const uploadResult = await handleUpload(req.files);
+        const extractedText = await extractTextPDF(uploadResult.path || "");
+        const data = parsePDFData(extractedText.text || "")
 
-    if(uploadResult.error)
-        return res.status(400).json({"error": uploadResult.error});
-
-    const extractedText = await extractTextPDF(uploadResult.path || "");
-    if(extractedText.error)
-        return res.status(400).json({"error": extractedText.error});
-
-    const data = parsePDFData(extractedText.text || "")
-
-    return res.status(200).json(data);
+        return res.status(200).json(data);
+    } catch (e) {
+        console.log(e)
+        return res.status(400).json();
+    }
 })
 
 export default apiRouter;
