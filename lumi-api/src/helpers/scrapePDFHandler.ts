6c67558cd63ca3b88e4e1900e171b415b05c0e2f
guilhermigg <guilhermigg.e@gmail.com>
2024-05-25 07:52:44 -0300

    change: estratégia diferente de error handling

--- lumi-api/src/helpers/scrapePDFHandler.ts
+++ lumi-api/src/helpers/scrapePDFHandler.ts
@@ -3,17 +3,11 @@ import PDFParser from 'pdf-parse';
 
 interface ITextPDF {
     text?: string,
-    error?: string
 }
 
 export default async function extractTextPDF(pdfPath: string) : Promise<ITextPDF> {
-    try{
-        const pdfBuffer = fs.readFileSync(pdfPath);
-        const pdf = await PDFParser(pdfBuffer);
-        const text = pdf.text;
-        return {"text": text};
-    } catch (e) {
-        console.log("[PDF Parser]", e)
-        return {"error": "Could not parse PDF file"}
-    }
+    const pdfBuffer = fs.readFileSync(pdfPath);
+    const pdf = await PDFParser(pdfBuffer);
+
+    return {"text": pdf.text};
 }
