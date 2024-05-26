bef5bc1b603fc93eaca7c697b038b2186c2d97b6
guilhermigg <guilhermigg.e@gmail.com>
2024-05-26 13:45:41 -0300

    url do teste de healthcheck atualizada

--- lumi-api/src/tests/app.test.ts
+++ lumi-api/src/tests/app.test.ts
@@ -4,7 +4,7 @@ describe('API Healthcheck', () => {
   let response : Response;
 
   beforeAll(async () => {
-    response = await fetch('http://localhost:5000/healthcheck');
+    response = await fetch('http://localhost:5000/api/v1/healthcheck');
   });
 
   test('Status do Healthcheck deve ser 200 OK', () => {
