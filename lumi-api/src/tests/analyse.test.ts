5f645390e308d54b67b50854881f1f57cd70d897
guilhermigg <guilhermigg.e@gmail.com>
2024-05-27 20:36:58 -0300

    update: test de analyse

--- lumi-api/src/tests/analyse.test.ts
+++ lumi-api/src/tests/analyse.test.ts
@@ -1,36 +1,34 @@
-
 import extractTextPDF from '../helpers/scrapePDFHandler';
 import parsePDFData from '../helpers/parsePDFData';
 
-import { describe, expect, beforeAll, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
-describe("Testando extração de dados da Fatura", () => {
+describe("extração de dados da fatura", () => {
     let pdfText : string | undefined;
+
     let expectedPdfData : IPDFData = {
             customerNumber: "7005400387",
             electricity: {
-                "kwh": "622",
-                "price": "0,74860466",
+                "kwh": 622,
+                "price": 465.66,
             },
             electricityGDI: {
-                "kwh": "767",
-                "price": "767",
+                "kwh": 767,
+                "price": -500.89,
             },
             electricitySCEE: {
-                "kwh": "767",
-                "price": "0,65313000",
+                "kwh": 767,
+                "price": 500.89,
             },
-            municipalPublicLightingContribution: "43,10",
-            referenceMonth: "Janeiro/2023",
+            municipalPublicLightingContribution: 43.1,
+            referenceMonth: new Date('2023-01-01T03:00:00.000Z')
         }
-
-    beforeAll(() => {
-
-    })
 
     test('Ler arquivo PDF', async () => {
         const result = await extractTextPDF(__dirname + '/data/fatura_1.pdf'); 
+
         pdfText = result.text;
+
         expect(pdfText).toBeDefined();
         expect(pdfText?.trim().split(' ')[0]).toBe("Valores");
     });
