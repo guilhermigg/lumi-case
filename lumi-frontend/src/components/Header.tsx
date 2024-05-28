efc86931bd7ba21eaa8c14398ccfd8491d76b95e
guilhermigg <guilhermigg.e@gmail.com>
2024-05-27 21:57:38 -0300

    header: atualizado

--- lumi-frontend/src/components/Header.tsx
+++ lumi-frontend/src/components/Header.tsx
@@ -1,21 +1,51 @@
+import { Button } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
 
 export default function Header({title}: {title: string}) {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar component="nav">
         <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
           <Box sx={{padding: 1}}>
             <img src="https://uploads-ssl.webflow.com/62f9249c43126cafce10bc33/62fd12497ffcb83b28ea3309_logo-lumi-white.svg" width={110} />
           </Box>
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <Button sx={{ color: '#fff' }}>
+                <Link to="/dashboard" style={{color: 'white', textDecoration: 'none', textDecorationColor: 'white'}}>
+                  Dashboard
+                </Link>
+              </Button>
+
+              <Button sx={{ color: '#fff' }}>
+                <Link to="/library" style={{color: 'white', textDecoration: 'none', textDecorationColor: 'white'}}>
+                  Faturas
+                </Link>
+              </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <AppBar position="static">
+        <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {title}
+            <Link to="/dashboard"> Dashboard </Link>
+          </Typography>
+
+          <Typography variant="h6">
+            <Link to="/dashboard"> Dashboard </Link>
           </Typography>
         </Toolbar>
       </AppBar>
     </Box>
   );
+
 }
